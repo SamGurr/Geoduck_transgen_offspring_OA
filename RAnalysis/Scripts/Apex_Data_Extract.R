7fdfd8caf95772831b46f74a42959ed8f318d392
SamJGurr <samjgurr@gmail.com>
2019-02-20 14:07:18 -0800

    new apex script for different apex IP address and test datasets

--- RAnalysis/Scripts/Apex_Data_Extract.R
+++ RAnalysis/Scripts/Apex_Data_Extract.R
@@ -1,13 +1,13 @@
 #http://www.informit.com/articles/article.aspx?p=2215520
 
-#modified as of 20190124 SJG
+#modified as of 20190217 SJG
 #added reminders on lines 39 and 43 to prevent overwritting files ->  SJG
 #changes in lines 20 - 36 for column name changes for switched conicals -> 20170708 by SJG 
 
 library("XML")
 library("plyr")
 
-xmlfile <- xmlParse("http://192.168.1.201:80/cgi-bin/datalog.xml?sdate=190206&days=3") #read in the date plus x days of Apex data
+xmlfile <- xmlParse("http://192.168.1.200:80/cgi-bin/datalog.xml?sdate=190219&days=1") #read in the date plus x days of Apex data
 
 Apex.Data <- ldply(xmlToList(xmlfile), data.frame) #convert xml to dataframe
 
@@ -16,43 +16,72 @@ Apex.Data2 <- head(Apex.Data2,-2) #remove extra metadata from bottom
 
 # view data and names to ID the raw probe.name or probe.type or probe.value
 Apex.Data2
+tail(Apex.Data2) # check most recent data 
 names(Apex.Data2)
 
-# Column names modified as of 20190129
-# Date.Time = column 2
-# TMP_T0 = column 6
-# pH_T0= column 9
-# salinity = column 12
-# TMP_T2 = column 66
-# pH_T2 = column 69
-# TMP_T1 = column 72
-# pH_T1 = column 75
-# TMP_T3 = column 78
-# pH_T3 = column 81
-# NOTE: 10 in total above
+# Column names modified as of 20190220 - new apex (from broodstock set-up) moved to conicals due to a malfunction
+
+# Date.Time = column 3
+
+# TMP_T0 = column 6 
+# pH_T0= column 9 
+
+# TMP_T4 = column 69 
+# pH_T4 = column 72 
+
+# TMP_T6 = column 75 
+# pH_T6 = column 78 
+
+# TMP_T5 = column 81 
+# pH_T5 = column 84  
+
+# TMP_T1 = column 87 
+# pH_T1 = column 90 
+
+# TMP_T7 = column 93 
+# pH_T7 = column 96 
+
+# TMP_T3 = column 99 
+# pH_T3 = column 102 
+
+# TMP_T2 = column 105 
+# pH_T2 = column 108 pH_T2
+
+# NOTE: 18 in total above
 
 #keep columnes with data of interest. This needs to be changed as it will be specific to the Apex configuration
-Probe.Data <- Apex.Data2[,c(3,6,9,12,66, 69, 72, 75, 78, 81)] #select columns
-colnames(Probe.Data) <- c("Date.Time", "TMP_T0", "pH_T0", "Sal", "TMP_T2", "pH_T2","TMP_T1",
-                           "pH_T1", "TMP_T3", "pH_T3")  #rename columns
+Probe.Data <- Apex.Data2[,c(3,6,9,69,72,75,78,81,84,87,90,93,96,99,102,105,108)] #select columns
+colnames(Probe.Data) <- c("Date.Time", "TMP_T0", "pH_T0", "TMP_T4", "pH_T4", "TMP_T6","pH_T6",
+                           "TMP_T5", "pH_T5", "TMP_T1", "pH_T1", "TMP_T7", "pH_T7", "TMP_T3", 
+                          "pH_T3", "TMP_T2", "pH_T2")  #rename columns
 Probe.Data$Date.Time <- as.POSIXct(Probe.Data$Date.Time, format = "%m/%d/%Y %H:%M:%S", tz="HST") #convert date to HI time
+# tail(Probe.Data) # to view the newest data and compare to APEX fusion for assigning column names
 # CHANGE DATE FOR NEW CSV (risk overwritting previous)
-write.csv(Probe.Data, "C:/Users/samjg/Documents/My_Projects/Geoduck_transgen_offspring_OA/RAnalysis/Data/Apex_data/Output/20190209_Apex_Data_Output.data.csv") #write file to save data
+write.csv(Probe.Data, "C:/Users/samjg/Documents/My_Projects/Geoduck_transgen_offspring_OA/RAnalysis/Data/Apex_data/Output/201902120_Apex_Data_Output.data.TEST.csv") #write file to save data
 
 #plot Temp and pH and save to output
 # CHANGE DATE FOR NEW PDF (risk overwritting previous)
-pdf("C:/Users/samjg/Documents/My_Projects/Geoduck_transgen_offspring_OA/RAnalysis/Data/Apex_data/Graphs/20190209_Apex_Data_Output.data.pdf")
+pdf("C:/Users/samjg/Documents/My_Projects/Geoduck_transgen_offspring_OA/RAnalysis/Data/Apex_data/Graphs/20190220_Apex_Data_Output.data.TEST.pdf")
 par(mfrow=c(2,1))
 plot(as.numeric(as.character(TMP_T0)) ~ Date.Time, Probe.Data, col = "grey", type="l", ylim=c(10, 20),  xlab="Time", ylab="Temperature °C")
 lines(as.numeric(as.character(TMP_T1)) ~ Date.Time, Probe.Data, col = "red")
 lines(as.numeric(as.character(TMP_T2)) ~ Date.Time, Probe.Data, col = "blue")
 lines(as.numeric(as.character(TMP_T3)) ~ Date.Time, Probe.Data, col = "black")
+lines(as.numeric(as.character(TMP_T4)) ~ Date.Time, Probe.Data, col = "green")
+lines(as.numeric(as.character(TMP_T5)) ~ Date.Time, Probe.Data, col = "purple")
+lines(as.numeric(as.character(TMP_T6)) ~ Date.Time, Probe.Data, col = "orange")
+lines(as.numeric(as.character(TMP_T7)) ~ Date.Time, Probe.Data, col = "brown")
+
 axis.POSIXct(side=1, Probe.Data$Date.Time)
 
-plot(as.numeric(as.character(pH_T0)) ~ Date.Time, Probe.Data, col = "grey", type="l", ylim=c(6.5, 8.1),  xlab="Time", ylab="pH NBS")
+plot(as.numeric(as.character(pH_T0)) ~ Date.Time, Probe.Data, col = "grey", type="l", ylim=c(3.5, 8.5),  xlab="Time", ylab="pH NBS")
 lines(as.numeric(as.character(pH_T1)) ~ Date.Time, Probe.Data, col = "red")
 lines(as.numeric(as.character(pH_T2)) ~ Date.Time, Probe.Data, col = "blue")
 lines(as.numeric(as.character(pH_T3)) ~ Date.Time, Probe.Data, col = "black")
+lines(as.numeric(as.character(pH_T4)) ~ Date.Time, Probe.Data, col = "green")
+lines(as.numeric(as.character(pH_T5)) ~ Date.Time, Probe.Data, col = "purple")
+lines(as.numeric(as.character(pH_T6)) ~ Date.Time, Probe.Data, col = "orange")
+lines(as.numeric(as.character(pH_T7)) ~ Date.Time, Probe.Data, col = "brown")
 axis.POSIXct(side=1, Probe.Data$Date.Time)
 
 # plot(as.numeric(as.character(Salt_XL)) ~ Date.Time, Probe.Data, col = "grey", type="l", ylim=c(20, 35),  xlab="Time", ylab="Salinity psu")
